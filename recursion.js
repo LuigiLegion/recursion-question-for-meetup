c89c45047fc186502eb2ec20f7b6bc2b0e9fe49b
Tal Luigi <luigi.t.agr@gmail.com>
2019-05-28 12:34:04 -0400

    solved isPalindrome

--- recursion.js
+++ recursion.js
@@ -1,3 +1,15 @@
-function isPalindrome(string) {
+function isPalindrome(str) {
   //your code here
+  const casedStr = str.toLowerCase();
+  const spacedCasedStr = casedStr.split(' ').join('');
+  if (spacedCasedStr.length <= 1) {
+    return true;
+  } else if (
+    spacedCasedStr.length > 1 &&
+    spacedCasedStr[0] !== spacedCasedStr[spacedCasedStr.length - 1]
+  ) {
+    return false;
+  } else {
+    return isPalindrome(spacedCasedStr.slice(1, -1));
+  }
 }
